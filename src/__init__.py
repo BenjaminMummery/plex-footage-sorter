de77aaeb2f3d52653a2af4b5e91ae77bd0442e44
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-16 14:03:25 +0000

    ci: semantic-release setup. (#4)

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
 # Copyright (c) 2023 Benjamin Mummery
 
 """Initialiser to make phone_footage_sorter accessible externally."""
+
+# Version number, automatically updated by semantic-release
+__version__ = "0.0.0"
