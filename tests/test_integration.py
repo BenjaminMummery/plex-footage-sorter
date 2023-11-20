68745c0ee272d453beae2bde690967275c64e206
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-20 20:59:54 +0000

    test: add test for single file renaming.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -16,3 +16,20 @@ class TestNullCases:
         # WHEN
         with cwd(tmp_path):
             main()
+
+
+class TestSingleFiles:
+    @staticmethod
+    def test_renames_correctly(tmp_path: Path, cwd, mocker: MockerFixture):
+        # GIVEN
+        filename = "20220202_222222.mp4"
+        (tmp_path / filename).write_text("<sentinel>")
+        mocker.patch("sys.argv", ["stub_name"])
+
+        # WHEN
+        with cwd(tmp_path):
+            main()
+
+        # THEN
+        with open(tmp_path / "Training Videos - 2022-02-02.mp4", "r") as f:
+            assert f.read() == "<sentinel>"
