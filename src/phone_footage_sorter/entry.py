1a2cff2b20f5f56d90180cdfebea9a69e009fe97
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-21 16:51:54 +0000

    feat: add custom naming.

--- src/phone_footage_sorter/entry.py
+++ src/phone_footage_sorter/entry.py
@@ -2,12 +2,23 @@
 
 """Process android phone footage default names into plex-friendly filename."""
 
+import argparse
 import os
 from datetime import datetime
 
 
 def main():
     """Search the dir for matching files and rename them."""
+
+    # Parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "title",
+        type=str,
+        help="A custom name to be attached to the start of the new file names.",
+    )
+    args = parser.parse_args()
+
     days: dict = {}
     for file in os.listdir():
         try:
@@ -26,7 +37,7 @@ def main():
         if len(files := days[day]) == 1:
             os.rename(
                 files[0],
-                "Training Videos - "
+                f"{args.title} - "
                 f"{day.year}-{day.month:02d}-{day.day:02d}"
                 f"{os.path.splitext(file)[1]}",
             )
@@ -34,7 +45,7 @@ def main():
             for i, file in enumerate(sorted(files)):
                 os.rename(
                     file,
-                    "Training Videos - "
+                    f"{args.title} - "
                     f"{day.year}-{day.month:02d}-{day.day:02d} - Part{i+1}"
                     f"{os.path.splitext(file)[1]}",
                 )
