e9195ecfe7b7e77f53ae402281d812d00455cf87
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-21 09:59:31 +0000

    test: add system tests for flat folder.

--- tests/test_system.py
+++ tests/test_system.py
@@ -5,6 +5,24 @@ import os
 
 class TestSetup:
     @staticmethod
-    def test_install(virtualenv):
-        virtualenv.run("pwd")
-        virtualenv.run(f"python -m pip install {os.getcwd()}")  # noqa: E501
+    def test_install(virtualenv, tmp_path):
+        virtualenv.run(f"python -m pip install {os.getcwd()}")
+        virtualenv.run(f"cd {tmp_path} && phone-footage-sorter")
+
+
+class TestRun:
+    @staticmethod
+    def test_flat_folder(virtualenv, tmp_path):
+        infiles = ["20220202_222222.mp4", "20220203_222222.mp4", "20220203_222223.mp4"]
+        outfiles = [
+            "Training Videos - 2022-02-02.mp4",
+            "Training Videos - 2022-02-03 - Part1.mp4",
+            "Training Videos - 2022-02-03 - Part2.mp4",
+        ]
+        for file in infiles:
+            (tmp_path / file).write_text("")
+
+        virtualenv.run(f"python -m pip install {os.getcwd()}")
+        virtualenv.run(f"cd {tmp_path} && phone-footage-sorter")
+
+        assert set(os.listdir(tmp_path)) == set(outfiles)
