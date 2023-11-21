a49e1eba1aa913885628a8b718b4d076cdad7102
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-21 16:49:40 +0000

    test: add tests for custom naming.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,6 +3,7 @@
 import os
 from pathlib import Path
 
+import pytest
 from pytest_mock import MockerFixture
 
 from src.phone_footage_sorter.entry import main
@@ -12,37 +13,40 @@ class TestNullCases:
     @staticmethod
     def test_no_files(tmp_path: Path, cwd, mocker: MockerFixture):
         # GIVEN
-        mocker.patch("sys.argv", ["stub_name"])
+        mocker.patch("sys.argv", ["stub_name", "stub title"])
 
         # WHEN
         with cwd(tmp_path):
             main()
 
 
+@pytest.mark.parametrize("custom_name", ["Training Videos"])
 class TestFlatDir:
     @staticmethod
-    def test_renames_single_file_correctly(tmp_path: Path, cwd, mocker: MockerFixture):
+    def test_renames_single_file_correctly(
+        tmp_path: Path, cwd, mocker: MockerFixture, custom_name: str
+    ):
         # GIVEN
         filename = "20220202_222222.mp4"
         (tmp_path / filename).write_text("<sentinel>")
-        mocker.patch("sys.argv", ["stub_name"])
+        mocker.patch("sys.argv", ["stub_name", custom_name])
 
         # WHEN
         with cwd(tmp_path):
             main()
 
         # THEN
-        with open(tmp_path / "Training Videos - 2022-02-02.mp4", "r") as f:
+        with open(tmp_path / f"{custom_name} - 2022-02-02.mp4", "r") as f:
             assert f.read() == "<sentinel>"
 
     @staticmethod
     def test_renames_multiple_files_for_same_day(
-        tmp_path: Path, cwd, mocker: MockerFixture
+        tmp_path: Path, cwd, mocker: MockerFixture, custom_name: str
     ):
         # GIVEN
         for i, file in enumerate(["20220202_222222.mp4", "20220202_222223.mp4"]):
             (tmp_path / file).write_text(f"<{i} sentinel>")
-        mocker.patch("sys.argv", ["stub_name"])
+        mocker.patch("sys.argv", ["stub_name", custom_name])
 
         # WHEN
         with cwd(tmp_path):
@@ -51,15 +55,17 @@ class TestFlatDir:
         # THEN
         for i, file in enumerate(
             [
-                "Training Videos - 2022-02-02 - Part1.mp4",
-                "Training Videos - 2022-02-02 - Part2.mp4",
+                f"{custom_name} - 2022-02-02 - Part1.mp4",
+                f"{custom_name} - 2022-02-02 - Part2.mp4",
             ]
         ):
             with open(tmp_path / file, "r") as f:
                 assert f.read() == f"<{i} sentinel>"
 
     @staticmethod
-    def test_ignores_non_matching_files(tmp_path: Path, cwd, mocker: MockerFixture):
+    def test_ignores_non_matching_files(
+        tmp_path: Path, cwd, mocker: MockerFixture, custom_name: str
+    ):
         # GIVEN
         for file in [
             "20220202_222222.mp4",
@@ -67,7 +73,7 @@ class TestFlatDir:
             "not_a_matching_file.mp4",
         ]:
             (tmp_path / file).write_text("")
-        mocker.patch("sys.argv", ["stub_name"])
+        mocker.patch("sys.argv", ["stub_name", custom_name])
 
         # WHEN
         with cwd(tmp_path):
@@ -75,7 +81,7 @@ class TestFlatDir:
 
         # THEN
         assert set(os.listdir(tmp_path)) == {
-            "Training Videos - 2022-02-02 - Part1.mp4",
-            "Training Videos - 2022-02-02 - Part2.mp4",
+            f"{custom_name} - 2022-02-02 - Part1.mp4",
+            f"{custom_name} - 2022-02-02 - Part2.mp4",
             "not_a_matching_file.mp4",
         }
--- tests/test_system.py
+++ tests/test_system.py
@@ -2,17 +2,20 @@
 
 import os
 
+import pytest
+
 
 class TestSetup:
     @staticmethod
     def test_install(virtualenv, tmp_path):
         virtualenv.run(f"python -m pip install {os.getcwd()}")
-        virtualenv.run(f"cd {tmp_path} && phone-footage-sorter")
+        virtualenv.run(f"cd {tmp_path} && phone-footage-sorter stub_title")
 
 
+@pytest.mark.parametrize("custom_name", ["'Training Videos'"])
 class TestRun:
     @staticmethod
-    def test_flat_folder(virtualenv, tmp_path):
+    def test_flat_folder(virtualenv, tmp_path, custom_name):
         infiles = ["20220202_222222.mp4", "20220203_222222.mp4", "20220203_222223.mp4"]
         outfiles = [
             "Training Videos - 2022-02-02.mp4",
@@ -23,6 +26,6 @@ class TestRun:
             (tmp_path / file).write_text("")
 
         virtualenv.run(f"python -m pip install {os.getcwd()}")
-        virtualenv.run(f"cd {tmp_path} && phone-footage-sorter")
+        virtualenv.run(f"cd {tmp_path} && phone-footage-sorter {custom_name}")
 
         assert set(os.listdir(tmp_path)) == set(outfiles)
