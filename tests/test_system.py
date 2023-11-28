06e7c8ab5b52c2d5f80a2d3e3edfb8412bbcf710
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-28 16:33:43 +0000

    test: expaded system tests.

--- tests/test_system.py
+++ tests/test_system.py
@@ -32,3 +32,62 @@ class TestRun:
 
         # THEN
         assert set(os.listdir(tmp_path)) == set(outfiles)
+
+    @staticmethod
+    def test_subfolder_no_recurse(virtualenv, tmp_path, custom_name):
+        # GIVEN
+        infiles = ["20220202_222222.mp4", "20220203_222222.mp4", "20220203_222223.mp4"]
+        outfiles = [
+            "Training Videos - 2022-02-02.mp4",
+            "Training Videos - 2022-02-03 - Part1.mp4",
+            "Training Videos - 2022-02-03 - Part2.mp4",
+        ]
+        subdir = "subdir"
+        (tmp_path / subdir).mkdir()
+        for file in infiles:
+            (tmp_path / file).write_text("")
+            (tmp_path / subdir / file).write_text("")
+
+        # WHEN
+        virtualenv.run(f"python -m pip install {os.getcwd()}")
+        virtualenv.run(f"cd {tmp_path} && plex-footage-sorter date-based {custom_name}")
+
+        # THEN
+        assert set(os.listdir(tmp_path)) == set(outfiles + [subdir])
+        assert set(os.listdir(tmp_path / subdir)) == set(infiles)
+
+    @staticmethod
+    def test_subfolder_recurse(virtualenv, tmp_path, custom_name):
+        # GIVEN
+        infiles = ["20220202_222222.mp4", "20220203_222222.mp4", "20220203_222223.mp4"]
+        infiles_subdir = [
+            "20230202_222222.mp4",
+            "20230203_222222.mp4",
+            "20230203_222223.mp4",
+        ]
+        outfiles = [
+            "Training Videos - 2022-02-02.mp4",
+            "Training Videos - 2022-02-03 - Part1.mp4",
+            "Training Videos - 2022-02-03 - Part2.mp4",
+        ]
+        outfiles_subdir = [
+            "Training Videos - 2023-02-02.mp4",
+            "Training Videos - 2023-02-03 - Part1.mp4",
+            "Training Videos - 2023-02-03 - Part2.mp4",
+        ]
+        subdir = "subdir"
+        (tmp_path / subdir).mkdir()
+        for file in infiles:
+            (tmp_path / file).write_text("")
+        for file in infiles_subdir:
+            (tmp_path / subdir / file).write_text("")
+
+        # WHEN
+        virtualenv.run(f"python -m pip install {os.getcwd()}")
+        virtualenv.run(
+            f"cd {tmp_path} && plex-footage-sorter date-based {custom_name} -r"
+        )
+
+        # THEN
+        assert set(os.listdir(tmp_path)) == set(outfiles + [subdir])
+        assert set(os.listdir(tmp_path / subdir)) == set(outfiles_subdir)
