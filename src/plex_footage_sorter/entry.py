97d1a1349f06c571df7e14e47b3971ea8a66bd81
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-28 12:14:16 +0000

    refactor: move date-based sorting to its own source file.

--- src/plex_footage_sorter/entry.py
+++ src/plex_footage_sorter/entry.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2023 Benjamin Mummery
 
-"""Process android phone footage default names into plex-friendly filename."""
+"""Rename and move files to conform to Plex conventions."""
 
 import argparse
 import os
-from datetime import datetime
+
+from . import _sort_dated_footage_as_date_series
 
 
 def main():
@@ -26,60 +27,9 @@ def main():
     )
     args = parser.parse_args()
 
-    print(f"Discovering files in {os.getcwd()}", end="")
-    if args.recursive:
-        print(" and subdirectories", end="")
-
-    days: dict = {}
-    if args.recursive:
-        files = [os.path.join(dp, f) for dp, _, fn in os.walk(os.getcwd()) for f in fn]
-    else:
-        files = os.listdir()
-
-    for file in files:
-        print(".", end="")
-        _, filename = os.path.split(file)
-
-        try:
-            if (
-                date := datetime.strptime(
-                    os.path.splitext(filename)[0], "%Y%m%d_%H%M%S"
-                ).date()
-            ) not in days:
-                days[date] = [file]
-            else:
-                days[date].append(file)
-        except ValueError:
-            continue
-
-    if len(days) == 0:
-        print("No files to rename, exiting.")
-        return
-
-    print("\nRenaming files:")
-    for day in days:
-        if len(files := days[day]) == 1:
-            dir, _ = os.path.split(files[0])
-            new_name = (
-                f"{args.title} - "
-                f"{day.year}-{day.month:02d}-{day.day:02d}"
-                f"{os.path.splitext(files[0])[1]}"
-            )
-            os.rename(
-                files[0],
-                os.path.join(dir, new_name),
-            )
-            print(f"  {file} --> {new_name}")
-        else:
-            for i, file in enumerate(sorted(files)):
-                dir, _ = os.path.split(file)
-                new_name = (
-                    f"{args.title} - "
-                    f"{day.year}-{day.month:02d}-{day.day:02d} - Part{i+1}"
-                    f"{os.path.splitext(file)[1]}"
-                )
-                os.rename(file, os.path.join(dir, new_name))
-                print(f"  {file} --> {new_name}")
+    return _sort_dated_footage_as_date_series.main(
+        os.getcwd(), args.title, recursive=args.recursive
+    )
 
 
 if __name__ == "__main__":
