695f01741aac6b1296c44c9dc220f95d612a41a1
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-28 10:33:05 +0000

    chore!: Package rename.

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2023 Benjamin Mummery
 
-"""Initialiser to make phone_footage_sorter accessible externally."""
+"""Initialiser to make plex_footage_sorter accessible externally."""
 
 # Version number, automatically updated by semantic-release
 __version__ = "1.2.0"
