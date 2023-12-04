12d0c5e4ddaa0eafa8a29df1e042caca86b7f6ea
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-12-04 14:19:31 +0000

    test: add test for multiple amazon-style files.

--- tests/test_integration_sort_movpilot_series.py
+++ tests/test_integration_sort_movpilot_series.py
@@ -18,7 +18,7 @@ class TestNullCases:
             main()
 
 
-class TestRestructure:
+class TestAmazonPattern:
     @staticmethod
     def test_single_file(tmp_path: Path, cwd, mocker: MockerFixture):
         # GIVEN
@@ -40,3 +40,51 @@ class TestRestructure:
         ) as f:
             assert f.read() == "<sentinel>"
         assert not old_subdir.exists()
+
+    @staticmethod
+    def test_multiple_files(tmp_path: Path, cwd, mocker: MockerFixture):
+        # GIVEN
+        series_name = "Star Trek (1966)"
+
+        # Create file structure
+        old_files = []
+        old_subdirs = []
+        (tmp_path / series_name).mkdir()
+        old_subdirs.append(tmp_path / series_name / "1")
+        old_subdirs[0].mkdir()
+        for file in ["S01E001_the_man_trap.mp4", "S01E002_charlie_x.mp4"]:
+            (old_file := (tmp_path / series_name / "1" / file)).write_text("<sentinel>")
+            old_files.append(old_file)
+        old_subdirs.append(tmp_path / series_name / "2")
+        old_subdirs[1].mkdir()
+        for file in ["S02E001_amok_time.mp4", "S02E002_who_mourns_for_adonais.mp4"]:
+            (old_file := (tmp_path / series_name / "2" / file)).write_text("<sentinel>")
+            old_files.append(old_file)
+
+        expected_files = [
+            tmp_path / series_name / "Season01" / file
+            for file in [
+                f"{series_name} - S01E01 - the man trap.mp4",
+                f"{series_name} - S01E02 - charlie x.mp4",
+            ]
+        ] + [
+            tmp_path / series_name / "Season02" / file
+            for file in [
+                f"{series_name} - S02E01 - amok time.mp4",
+                f"{series_name} - S02E02 - who mourns for adonais.mp4",
+            ]
+        ]
+
+        mocker.patch("sys.argv", ["stub_name", "movpilot-series"])
+
+        # WHEN
+        with cwd(tmp_path):
+            main()
+
+        for file in old_files:
+            assert not file.exists()
+        for subdir in old_subdirs:
+            assert not subdir.exists()
+        for file in expected_files:
+            with open(file) as f:
+                assert f.read() == "<sentinel>"
