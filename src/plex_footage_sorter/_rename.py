cd1d40c45e619f7e903e21fcdf41b41bbd2b90a8
Benjamin Mummery <benjamin.mummery13@gmail.com>
2024-01-08 15:15:52 +0000

    feat: handle args to rename.

--- src/plex_footage_sorter/_rename.py
+++ src/plex_footage_sorter/_rename.py
@@ -2,7 +2,17 @@
 
 """Platform independent wildcard rename command."""
 
+from pathlib import Path
 
-def main():
-    """Find and rename matching files."""
+
+def main(directory: Path, match_pattern: str, target_pattern: str, regex: bool):
+    """Find and rename matching files.
+
+    Args:
+        directory (Path): The directory to be searched.
+        match_pattern (str): The pattern to match to existing files.
+        target_pattern (str): The pattern to which matching files should be renamed.
+        regex (bool): If True, interpret the match and target patterns using regex. If
+            False, glob will be used. Defaults to False.
+    """
     pass
