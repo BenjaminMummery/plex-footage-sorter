37e32ede1ca036997bfde2124b1d9d14892f586c
Benjamin Mummery <benjamin.mummery13@gmail.com>
2024-01-09 14:57:39 +0000

    test: add test for ? wildcard.

--- tests/test_integration_rename.py
+++ tests/test_integration_rename.py
@@ -35,12 +35,30 @@ class TestNullCases:
 
 class TestGlob:
     @staticmethod
-    def test_renames_single_file_correctly_wildcard(
+    def test_renames_single_file_correctly_star_wildcard(
         tmp_path: Path, cwd, mocker: MockerFixture
     ):
         # GIVEN
         (tmp_path / (filename := "episode_1.mkv")).write_text("<sentinel>")
         mocker.patch("sys.argv", ["stub_name", "rename", "episode_*", "S01E0*"])
+
+        # WHEN
+        with cwd(tmp_path):
+            main()
+
+        # THEN
+        assert not (tmp_path / filename).exists()
+        assert (new_file := (tmp_path / "S01E01.mkv")).is_file()
+        with open(new_file) as f:
+            assert f.read() == "<sentinel>"
+
+    @staticmethod
+    def test_renames_single_file_correctly_question_mark_wildcard(
+        tmp_path: Path, cwd, mocker: MockerFixture
+    ):
+        # GIVEN
+        (tmp_path / (filename := "episode_1.mkv")).write_text("<sentinel>")
+        mocker.patch("sys.argv", ["stub_name", "rename", "episode_?.mkv", "S01E0?.mkv"])
 
         # WHEN
         with cwd(tmp_path):
