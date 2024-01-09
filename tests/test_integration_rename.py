a89c6dc4f2e61f12de7506004795560deae6b28c
Benjamin Mummery <benjamin.mummery13@gmail.com>
2024-01-09 15:40:02 +0000

    test: add test for multiple wildcards.

--- tests/test_integration_rename.py
+++ tests/test_integration_rename.py
@@ -158,6 +158,24 @@ class TestQuestionmarkWildcard:
             assert f.read() == "<sentinel>"
 
 
+class TestMixedWildcards:
+    @staticmethod
+    def test_rename_single_file(tmp_path: Path, cwd, mocker: MockerFixture):
+        # GIVEN
+        (tmp_path / (filename := "episode_1.mkv")).write_text("<sentinel>")
+        mocker.patch("sys.argv", ["stub_name", "rename", "episode_?.*", "S01E0?.*"])
+
+        # WHEN
+        with cwd(tmp_path):
+            main()
+
+        # THEN
+        assert not (tmp_path / filename).exists()
+        assert (new_file := (tmp_path / "S01E01.mkv")).is_file()
+        with open(new_file) as f:
+            assert f.read() == "<sentinel>"
+
+
 class TestFailureStates:
     @staticmethod
     def test_unsupported_glob_wildcard(tmp_path: Path, cwd, mocker: MockerFixture):
